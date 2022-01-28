12cd0891c6e92385eca45ba1468c216289d13e4c
KG_TeKoN <hasherama8@gmail.com>
2022-01-29 00:20:04 +0200

    Fimal version

---
+++ services/log.service.js
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = (error) => {
+    console.log(chalk.bgRed(' ERROR ') + ' ' + error);
+}
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+}
+
+const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan(' HELP ')} 
+        Without parameters - print weather
+        -s [CITY] for change city
+        -h for help
+        -t [API_KEY] for save token
+        `
+    );
+}
+
+const printWeather = (res, icon) => {
+    console.log(
+        dedent`${chalk.bgYellow(' WEATHER ')} weather in the city ${res.name}
+        ${icon} ${res.weather[0].description}
+        temperature: ${res.main.temp} feel like ${res.main.feels_like}
+        humidity: ${res.main.humidity}%
+        speed wind: ${res.wind.speed}
+        `
+    );
+}
+
+export { printError, printSuccess, printHelp, printWeather }
---
+++ weather.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from "./services/storage.service.js";
+import { getWeather, getIcon } from "./services/api.service.js";
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Token have not passed');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Token was saved');
+    } catch(e) {
+        printError(e);
+    }
+}
+
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('Town have not passed');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('Town was saved');
+    } catch(e) {
+        printError(e);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city)
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon))
+    } catch(e) {
+        if (e?.response?.status === 404) {
+            printError('uncorrectly city');
+        } else if (e?.response?.status === 401) {
+            printError('unccorectly token');
+        } else {
+            printError(e.message);
+        }
+    }
+}
+
+const initCLI = () => {
+    const args = getArgs(process.argv)
+    if (args.h) {
+        return printHelp();
+    }
+    if (args.s) {
+        return saveCity(args.s);
+    }
+    if (args.t) {
+        return saveToken(args.t);
+    }
+    return getForcast();
+}
+
+initCLI();
